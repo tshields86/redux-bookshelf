982db62e3a04f36e7f9f75e05c62a6dfe75d4c62
Travis Shields <travis.shields@gmail.com>
2016-06-07 19:56:50 -0400

    add book content

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,15 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts', pages: 101 },
-    { title: 'Harry Potter', pages: 820 },
-    { title: 'The Dark Tower', pages: 85 },
-    { title: 'Eloquent Ruby', pages: 75 }
+    { title: 'The Jungle Book', pages: 24 },
+    { title: 'Aladdin', pages: 26 },
+    { title: 'Walt Disney\'s Peter Pan', pages: 25 },
+    { title: 'Toy Story', pages: 22 },
+    { title: 'Snow White and the Seven Dwarfs', pages: 26 },
+    { title: 'Pinocchio', pages: 22 },
+    { title: 'The Little Mermaid', pages: 24 },
+    { title: 'Beauty and the Beast', pages: 25 },
+    { title: 'Bambi', pages: 28 },
+    { title: 'Walt Disney\'s Cinderella', pages: 22 },
+    { title: 'Sleeping Beauty', pages: 23 },
   ]
 }
